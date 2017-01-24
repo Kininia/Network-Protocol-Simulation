83380df17ca434496c20e006868f55d18b5da889
Kininia <wirelessrobo@gmail.com>
2017-01-24 21:34:20 +0100

    Added compiled file for CRVU and a folder to keep it in.

---
+++ OUR_CODE/CRVU/CRVU.cpp
@@ -0,0 +1,15 @@
+// CRVU.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <stdint.h>
+#include <bitset>
+
+using namespace std;
+int main() {
+	bitset<8> seed(string("00000010"));
+	unsigned int feedback = seed[0] ^ seed[1] ^ seed[2] ^ seed[3] ^ seed[4] ^ seed[5] ^ seed[6] ^ seed[7];
+	cout << feedback << endl;
+	return 0;
+}
+
