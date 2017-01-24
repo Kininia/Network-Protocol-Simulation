58360f641409e7038c8bd35d7e8a8e9bae724e6b
Kininia <wirelessrobo@gmail.com>
2017-01-24 23:08:10 +0100

    test2

--- OUR_CODE/CRVU/CRVU.cpp
+++ OUR_CODE/CRVU/CRVU.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main() {
 	bitset<8> seed(string("00000010"));
 	unsigned int feedback = seed[0] ^ seed[1] ^ seed[2] ^ seed[3] ^ seed[4] ^ seed[5] ^ seed[6] ^ seed[7];
-	cout << "hello world" << endl;
+	cout << feedback << endl;
 	return 0;
 }
 
