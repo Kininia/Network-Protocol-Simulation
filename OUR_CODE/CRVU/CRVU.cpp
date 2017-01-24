09ef67c4e522dd8a8c5bb80b4b611acf18c21ae0
root <root@localhost>
2017-01-24 22:57:21 +0100

    test

--- OUR_CODE/CRVU/CRVU.cpp
+++ OUR_CODE/CRVU/CRVU.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main() {
 	bitset<8> seed(string("00000010"));
 	unsigned int feedback = seed[0] ^ seed[1] ^ seed[2] ^ seed[3] ^ seed[4] ^ seed[5] ^ seed[6] ^ seed[7];
-	cout << feedback << endl;
+	cout << "hello world" << endl;
 	return 0;
 }
 
