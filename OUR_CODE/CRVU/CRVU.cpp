90442f335e8671e97195308769d139bec332096a
Kininia <wirelessrobo@gmail.com>
2017-01-25 13:48:34 +0100

    Commented a bit ifrån the code

--- OUR_CODE/CRVU/CRVU.cpp
+++ OUR_CODE/CRVU/CRVU.cpp
@@ -6,9 +6,11 @@
 #include <bitset>
 
 using namespace std;
+
 int main() {
 	bitset<8> seed(string("00000010"));
-	unsigned int feedback = seed[0] ^ seed[1] ^ seed[2] ^ seed[3] ^ seed[4] ^ seed[5] ^ seed[6] ^ seed[7];
+	cout << seed << endl;
+	unsigned int feedback = seed[0] ^ seed[1] ^ seed[2] ^ seed[3] ^ seed[4] ^ seed[5] ^ seed[6] ^ seed[7]; // Creates feedback bit from seed.
 	cout << feedback << endl;
 	return 0;
 }
