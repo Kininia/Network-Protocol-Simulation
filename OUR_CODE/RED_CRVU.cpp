c556c185ce2ca3efea9a48234280e9d32c67ca68
Unknown <wirelessrobo@gmail.com>
2017-01-30 11:56:00 +0100

    RED_RPDU done!
    RED_RPDU works in conjunction with RED_RPDU.

---
+++ OUR_CODE/RED_CRVU.cpp
@@ -0,0 +1,21 @@
+#include <random>
+#include <iostream>
+#include <stdint.h>
+#include <bitset>
+#include "RED_CRVU.h"
+
+using namespace std;
+
+/*int main() {
+	
+	 cout << randomNumber() << endl;
+	
+	return 0;
+}*/
+
+	double RED_CRVU::randomNumber(){
+		random_device rd;
+		mt19937 gen(rd());
+		uniform_real_distribution<> dis(0, 1);
+		return dis(gen);
+	}
---
+++ OUR_CODE/RED_RPDU.cpp
@@ -0,0 +1,30 @@
+#include <stdint.h>
+#include <iostream>
+
+#include "RED_CRVU.h"
+#include "RED_RPDU.h"
+
+
+using namespace std;
+
+	void RED_RPDU::calculate() {
+		RED_CRVU CRVU;
+		//RED_PDPU PDPU;
+		//pdrop = PDPU.get_PB();
+		q = CRVU.randomNumber();
+		cout << q << endl;
+		/*if (q <= pdrop) {
+			discard_now = 1;
+		}
+		else {
+			discard_now = 0;
+		}*/
+	}
+
+//For testing class in conjunction with RED_CRVU
+int main() {
+
+	RED_RPDU RPDU;
+	RPDU.calculate();
+	return 0;
+}
